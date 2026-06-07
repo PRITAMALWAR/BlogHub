bc57291750a718b186537cd5c7c9c1bf0889a67d
PRITAMALWAR <pritamkumar08102004@gmail.com>
2026-06-07 12:01:24 +0530

    fix the cors error

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,24 +1,60 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8909",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Automatically attach JWT token
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
 
 export default api;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:8909",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// // Automatically attach JWT token
+// api.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default api;
